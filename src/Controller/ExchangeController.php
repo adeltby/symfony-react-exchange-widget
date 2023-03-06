8a466775f4fbdb4db6f6000dc85ef542f32d080f
Adel <Adeltby@gmail.com>
2023-03-06 20:44:53 +0330

    set up the backend

---
+++ src/Controller/ExchangeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExchangeController extends AbstractController
+{
+
+    #[Route('/api/exchange', name: 'app_exchange', methods: 'get')]
+    public function exchange()
+    {
+
+        $req_url = 'https://v6.exchangerate-api.com/v6/bd4108e87162f0fcd7df1bd4/latest/USD';
+
+        $response_json = file_get_contents($req_url);
+
+        if ($response_json) {
+
+            try {
+                $response = json_decode($response_json);
+
+                if ($response->result  === 'success') {
+
+                    $currencyValue = $response->conversion_rates;
+
+                    return $this->json(['data' => [
+                        "Emirates Derham (AED)" => $currencyValue->AED,
+                        "British Pound (GBP)" => $currencyValue->GBP,
+                        "Iranian Rial (IRR)" => $currencyValue->IRR,
+                        "Euro (EUR)" => $currencyValue->EUR,
+                        "Japanese Yen (JPY)" => $currencyValue->JPY,
+                    ]]);
+                }
+            } catch (Exception $e) {
+
+                return $this->json(['data' => $e]);
+            }
+        }
+    }
+}
---
+++ src/Controller/ReactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReactController extends AbstractController
+{
+
+        #[Route('/{reactRouting}', name: 'app_react', priority: '-1', defaults: ['reactRouting'=> null],
+    requirements: ['reactRouting'=> '.+'])]
+    public function index(): Response
+    {
+
+        return $this->render('index.html.twig', [
+            'controller_name' => 'ReactController',
+        ]);
+    }
+}
